42c8a5372d394569cad4d96f406a29057a977365
Birk Johannessen <>
2024-11-09 21:53:42 +0100

    prime: story telling, but better

--- src/components/HoverController.vue
+++ src/components/HoverController.vue
@@ -7,28 +7,31 @@
             <HoverText v-if="this.indexLevel == 5 && this.zoomLevel > 0" type="3" subText="Av Birk Johannessen" header="PrimeSpiral" />
         </Transition>
         <Transition>
-            <HoverText v-if="this.indexLevel == 7 && this.zoomLevel > 0" type="1" subText=".. i polarkoordinater" header="Visualisering av Primtall" />
+            <HoverText v-if="this.indexLevel == 7 && this.zoomLevel > 0" type="1" subText="... i polarkoordinater" header="Visualisering av primtall" />
         </Transition>
         <Transition>
             <HoverText v-if="this.indexLevel == 9 && this.zoomLevel > 0" type="2" subText="Hver prikk representerer et primtall" header="Hmm?" />
         </Transition>
         <Transition>
-            <HoverText v-if="this.indexLevel == 11 && this.zoomLevel > 0" type="1" subText=".. Som er primtallets lengde fra sentrum .." header="" />
+            <HoverText v-if="this.indexLevel == 11 && this.zoomLevel > 0" type="1" subText="... Som er sin egen lengde fra sentrum ..." header="Hmm?" />
         </Transition>
         <Transition>
-            <HoverText v-if="this.indexLevel == 13 && this.zoomLevel > 0" type="2" subText=".. og vinklet i primtallets radian!" header="" />
+            <HoverText v-if="this.indexLevel == 13 && this.zoomLevel > 0" type="2" subText="... og vinklet i sin egen radian." header="Hmm?" />
         </Transition>
         <Transition>
-            <HoverText v-if="this.indexLevel == 15 && this.zoomLevel > 0" type="4" subText="Det former dette kule mønsteret!!" header="1, 3, 5, 7, 9, 13" />
+            <HoverText v-if="this.indexLevel == 15 && this.zoomLevel > 0" type="4" subText="... Former mønsteret" header="2, 3, 5, 7, 13, 17, 19, 23, 29" />
         </Transition>
         <Transition>
-            <HoverText v-if="this.indexLevel == 17" type="1" subText="Er hvor mange primtall du ser akkurat nå! (høyeste primtallet her er 524.287)" header="43.390" />
+            <HoverText v-if="this.indexLevel == 17" type="4" subText="(17, 9 øverst) Er antall primtall du ser akkurat nå. (høyeste med verdi 524.287)" header="43.390" />
         </Transition>
         <Transition>
-            <HoverText v-if="this.indexLevel == 19 && this.zoomLevel > 0" type="2" subText="Originalt et python script som bare laget bilde du ser .." header="" />
+            <HoverText v-if="this.indexLevel == 18" type="2" subText="Er ganske kult. Det er 2^19-1 og derfor binært 0b1111111111111111111" header="Primtallet 524.287" />
         </Transition>
         <Transition>
-            <HoverText v-if="this.indexLevel == 20 && this.zoomLevel > 0" type="3" subText=".. Omgjort til et webskaleringsprosjekt!" header="" />
+            <HoverText v-if="this.indexLevel == 19 && this.zoomLevel > 0" type="1" subText="Originalt et python script generete primespiral bilder ..." header="PrimeSpiral" />
+        </Transition>
+        <Transition>
+            <HoverText v-if="this.indexLevel == 20 && this.zoomLevel > 0" type="3" subText="... transformert dette interaktive webprosjektet!" header="PrimeSpiral" />
         </Transition>
     </div>
   
