7e7b953ba59b3100acd8c0389a9655a70fa00d7c
Birk Johannessen <>
2025-04-07 15:43:20 +0200

    refactor: timeline component functional with config

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,3 +1,9 @@
+<script setup>
+    import { computed } from 'vue'; 
+    import Config from '@/config/timelineConfig.js'
+    const timeline = computed(() => Config.timeline);
+</script>
+
 <template>
     <div class="timeline-wrapper">
         <div class="center-padding"></div>
@@ -15,100 +21,6 @@
         </div>
     </div>
 </template>
-
-<script>
-    export default {
-    name: 'Timeline',
-    data() {
-        return {
-            timeline: [
-                {
-                    time: '2020 / August',
-                    title: '',
-                    description: 'Første Hello world program.',
-                    ghostcription: 'public static void Main(String Args[]) {\n     System.out.println("Hello World!");\n}'
-                },
-                /*
-                {
-                    time: '2022/Januar',
-                    title: '',
-                    description: 'Første sideprosjekt, ROOMBOT, et python program for å automatisk booke grupperom på høyskolen',
-                    ghostcription: '{\nbackend: ["php", "MySql", "nginx", "Python", "Crontab", "Ubuntu server"],    \nfrontend: ["php"]    \n}'
-                },
-                */
-                {
-                    time: '2022/November',
-                    title: '',
-                    description: 'Ansatt som Systemutvikler @Machina, deltid',
-                    ghostcription: '{\n    backend: ["Java", "PostGreSQL"],    \n    frontend: ["Backbone.js"]    \n}'
-                },
-                {
-                    time: '2022/Desember',
-                    title: '',
-                    description: 'Fullført internship @Link utvikling',
-                    ghostcription: ''
-                },
-                {
-                    time: '2023/April',
-                    title: '',
-                    description: 'Bachelorprosjekt - utviklet leetcode lignende applikasjon @wa.works fra scratch.',
-                    ghostcription: '{\nbackend: ["C#", ".ASP.NET", "SignalR", "Hangfire", "Entity framework", "PostGreSQL"],     \nfrontend: ["React.js"]     \n}'
-                },
-                {
-                    time: '2023/Mai',
-                    title: '',
-                    description: 'Ansatt som Fullstackutvikler @Machina, heltid',
-                    ghostcription: '{\n    backend: ["Java", "PostGreSQL"],\n    frontend: ["backbone.js", "jquery.js"]\n}'
-                },
-                {
-                    time: '2023/Juni',
-                    title: '',
-                    description: 'Uteksaminert fra HVL som Dataingeniør',
-                    ghostcription: '{\nnull: "🎓"    \n}'
-                },
-                /*
-                {
-                    time: '2023/2024',
-                    title: '',
-                    description: 'Synkronisere serverstate med utgående meldinger\n som epost, SMS og andre forsendelser som SvarUt.',
-                    ghostcription: '{\n    project: "Meldingskø",\n    omfang: "Stor refactor for ny kodesti for samtlige forsendelser for 29~ kunder"\n    tags: ["legacy", "refactor", "mimemessage", "FIFO queue"]\n}'
-                },
-                {
-                    time: '2024',
-                    title: '',
-                    description: 'Utviklet nytt beslutningssystem på saker, og dokumenter.',
-                    ghostcription: '{\n    project: "Decision",     \ntags: ["backend"]     \n}'
-                },
-                {
-                    time: '2024/Mai',
-                    title: '',
-                    description: 'Betalingsløsning for FBT knyttet Machinas løsning for dem.\nIntegrasjon mot NETS.',
-                    ghostcription: '{\n    project: "NETS integration",     \ntags: ["Integration", "API", "NETS"]     \n}'
-                },
-                {
-                    time: '2024/August',
-                    title: '',
-                    description: 'Utviklet Ny kilde til å generere smarte dokumenter',
-                    ghostcription: '{\n    project: "Docengine",     \ntags: ["Backend", "Java", "Documents", "Engine"]     \n}'
-                },
-                {
-                    time: '2024/November',
-                    title: '',
-                    description: 'Dokumentmotor for å håndtere tilstanden til dokumenter i ulike faser',
-                    ghostcription: '{\n    project: "Docengine",     \ntags: ["Backend", "Java", "Documents", "Engine"]     \n}'
-                },
-                {
-                    time: '2024/Desember',
-                    title: '',
-                    description: 'Godkjennings fase knyttet til dokumenter (Bakgrunn for dokumentmotor)',
-                    ghostcription: '{\n    project: "Docengine",     \ntags: ["Fullstack", "Documents", "Approval"]     \n}'
-                },
-                */
-            ]
-        }
-    },
-}
-</script>
 
 <style lang="scss" scoped>
     @import '@/assets/stylesheets/all.scss';
