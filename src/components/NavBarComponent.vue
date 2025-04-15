25b8dfebd646a903cbb03655fa334df413ed71cd
Birk Johannessen <>
2025-04-15 19:43:13 +0200

    tsified + vue3 navbar

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,42 +1,62 @@
 <script setup lang="ts">
-    import { onMounted, onUnmounted, ref } from 'vue';
+    import { onMounted, onUnmounted, ref, watch } from 'vue';
     import type { Ref } from 'vue';
-    import { useRouter  } from 'vue-router';
+    import { useRouter, useRoute  } from 'vue-router';
     import { navbarpaths } from '@/config/NavbarConfig';
-    import type NavbarPath from '@/models/NavbarPath';
+    import NavbarPath from '@/models/NavbarPath';
 
     const router = useRouter();
+    const route = useRoute();
 
     const showHamburger: Ref<boolean> = ref(false);
     const scrollY: Ref<number> = ref(0);
-    const paths: Ref<number> = ref(0);
+    const navPaths: Ref<NavbarPath[]> = ref(navbarpaths);
 
-    const updateY = () => {
+    const updateY = () : void => {
         scrollY.value = window.top?.scrollY || 0;
     }
 
+    const updateSelected = () : void => {
+        navPaths.value.forEach((navPath: NavbarPath) => {
+             navPath.selected = false;
+        }); 
+
+        const selectedPath: NavbarPath | undefined = navPaths.value.find((navPath: NavbarPath) => route.path === navPath.path);
+
+        if (selectedPath) {
+            selectedPath.selected = true;
+            return;
+        }
+
+        const selectedPathMatch: NavbarPath | undefined = navPaths.value
+            .filter((navPath: NavbarPath) => navPath.path)
+            .find((navPath: NavbarPath) => navPath.matchOn.some((m: string) => route.path.match(m)));
+
+        if (selectedPathMatch) {
+            selectedPathMatch.selected = true;
+        }
+    };
+
+    watch(() => route.path, () => updateSelected());
+
     onMounted(() => {
         window.addEventListener('scroll', updateY);
+        updateSelected();
     });
 
     onUnmounted(() => {
         window.removeEventListener('scroll', updateY);
     });
 
-    const push = (navbarPath: NavbarPath) => {
+    const push = (navbarPath: NavbarPath) : void => {
         router.push({ path: navbarPath.path });
     };
 
-    const pushRoot = () => {
+    const pushRoot = () : void => {
         router.push({ path: '/' });
     };
 
-    const isSelected = (navbarPath: NavbarPath) => {
-        if (navbarPath.matchOn && navbarPath.matchOn.length && navbarPath.matchOn.some((o: string) => window.location.pathname.match(o))) return true;
-        return window.location.pathname === navbarPath.path;
-    };
-    
-    const toggleHamburger = () => {
+    const toggleHamburger = () : void => {
         showHamburger.value = !showHamburger.value;
     }
 </script>
@@ -49,9 +69,9 @@
             </a>
         </div>
         <div class="path-wrapper">
-            <nav v-for="path in navbarpaths" class="hide-mb">
+            <nav v-for="path in navPaths" class="hide-mb">
                 <a class="tooltip-holder" @click="push(path)">
-                    <span class="material-symbols-outlined" :class="isSelected(path) ? 'selected' : ''">{{path.icon}}</span>
+                    <span class="material-symbols-outlined" :class="path.selected ? 'selected' : ''">{{path.icon}}</span>
                     <div class="tooltip tright">{{path.name}}</div>
                 </a>
             </nav>
@@ -63,10 +83,10 @@
         </div>
     </div>
     <div class="hamburger-overlay hide-desktop" v-if="showHamburger">
-        <nav v-for="path in navbarpaths">
+        <nav v-for="path in navPaths">
             <a @click="push(path); toggleHamburger()">
-                <span class="material-symbols-outlined" :class="isSelected(path) ? 'selected' : ''">{{path.icon}}</span>
-                <h4 :class="isSelected(path) ? 'selected' : ''">{{path.name}}</h4>
+                <span class="material-symbols-outlined" :class="path.selected ? 'selected' : ''">{{path.icon}}</span>
+                <h4 :class="path.selected ? 'selected' : ''">{{path.name}}</h4>
             </a>
         </nav>
     </div>
--- src/models/NavbarPath.ts
+++ src/models/NavbarPath.ts
@@ -3,16 +3,19 @@ export default class NavbarPath {
     public path: string;
     public icon: string;
     public matchOn: string[];
+    public selected: boolean
 
     constructor(
         name: string,
         path: string,
         icon: string,
-        matchOn: string[]
+        matchOn: string[],
+        selected: boolean = false
     ) {
         this.name = name;
         this.path = path;
         this.icon = icon;
         this.matchOn = matchOn;
+        this.selected = selected;
     }
 }
